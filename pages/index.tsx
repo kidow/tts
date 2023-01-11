9a081f90c1d50efd20168646924c327f5a24d3ff
kidow <wcgo2ling@gmail.com>
2023-01-11 14:58:10 +0900

    web speech test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,32 @@ import type { NextPage } from 'next'
 interface State {}
 
 const HomePage: NextPage = () => {
-  return <div className="container mx-auto">HomePage</div>
+  const speak = (text: string) => {
+    if (!text) return
+
+    if (
+      typeof SpeechSynthesisUtterance === 'undefined' ||
+      typeof window.speechSynthesis === 'undefined'
+    ) {
+      alert('이 브라우저는 음성 합성을 지원하지 않습니다.')
+      return
+    }
+
+    window.speechSynthesis.cancel()
+
+    const speech = new SpeechSynthesisUtterance()
+    speech.rate = 1
+    speech.pitch = 1
+    speech.lang = 'ko-KR'
+    speech.text = text
+
+    window.speechSynthesis.speak(speech)
+  }
+  return (
+    <div className="container mx-auto">
+      <button onClick={() => speak('안녕하세요.')}>Speech</button>
+    </div>
+  )
 }
 
 export default HomePage
