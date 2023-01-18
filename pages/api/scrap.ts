a4bf6763da34bd436cfa0791942ddaa821beb455
kidow <wcgo2ling@gmail.com>
2023-01-18 13:44:52 +0900

    택도없나?

--- pages/api/scrap.ts
+++ pages/api/scrap.ts
@@ -4,12 +4,6 @@ import * as cheerio from 'cheerio'
 import chrome from 'chrome-aws-lambda'
 
 const IS_DEV = process.env.NODE_ENV === 'development'
-const executablePath =
-  process.platform === 'win32'
-    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-    : process.platform === 'linux'
-    ? '/usr/bin/google-chrome'
-    : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (!req.body.url) {
@@ -22,7 +16,13 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
     const browser = await puppeteer.launch({
       args: IS_DEV ? [] : chrome.args,
-      executablePath: IS_DEV ? executablePath : await chrome.executablePath,
+      executablePath: IS_DEV
+        ? process.platform === 'win32'
+          ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+          : process.platform === 'linux'
+          ? '/usr/bin/google-chrome'
+          : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        : await chrome.executablePath,
       headless: IS_DEV ? true : chrome.headless,
       ignoreHTTPSErrors: true
     })
